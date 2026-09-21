81fe1c7d3aae7623d0e32845731d3dcf6019a057
FeSantsss <tercetime18@gmail.com>
2026-09-21 19:41:18 -0300

    feat/photo sender created

---
+++ src/main/java/com/felipysantsss/telegram_amara_bot/utils/PhotoSender.java
@@ -0,0 +1,22 @@
+package com.felipysantsss.telegram_amara_bot.utils;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+public class PhotoSender {
+    public static void sendImage(String chatId, String url, TelegramClient telegramClient){
+        SendPhoto photoSender = SendPhoto
+                .builder()
+                .chatId(chatId)
+                .photo(new InputFile(url))
+                .build();
+
+        try {
+            telegramClient.execute(photoSender);
+        } catch (TelegramApiException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
