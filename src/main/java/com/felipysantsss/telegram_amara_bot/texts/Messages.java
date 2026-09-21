d1a0e7227ddf661cbf15c611a26dc6e9d9c8e9a8
Felipy Santos <tercetime18@gmail.com>
2026-09-21 17:08:35 -0300

    feat/add: buttons system and add: massage enums with atributes and getter for multi messages (#1)

---
+++ src/main/java/com/felipysantsss/telegram_amara_bot/texts/Messages.java
@@ -0,0 +1,33 @@
+package com.felipysantsss.telegram_amara_bot.texts;
+
+public enum Messages {
+    START_MESSAGE("Oii... cuidado pra não se perder aqui comigo \uD83D\uDE08\n" +
+            "\n" +
+            "Eu sou a Amara Eyess, 20 aninhos... \n" +
+            "loira, branquinha, carinhosa e toda safada \uD83E\uDEE3\n" +
+            "Pareço só uma menina doce e educada...\n" +
+            "mas quando a gente fica sozinho eu viro a putinha que obedece tudo o que você mandar \uD83D\uDD25\n" +
+            "\n" +
+            "\uD83D\uDC40 olha o que te espera no meu VIP.\n" +
+            "\n" +
+            "❤\uFE0F\u200D\uD83D\uDD25 Fotos de lingerie sexy no INTERESSADO \n" +
+            "❤\uFE0F\u200D\uD83D\uDD25 Fotinhas nuas no SAFADO\n" +
+            "❤\uFE0F\u200D\uD83D\uDD25 Vídeos exclusivos só para o VIP\n" +
+            "\n" +
+            "Aqui dentro...\n" +
+            "ninguém vê... ninguém ouve... e eu viro a puta tarada que você merece \uD83D\uDCA6\n" +
+            "\n" +
+            "Você aguenta entrar nesse jogo comigo?\n" +
+            "\n" +
+            "VEM PRO MEU CANTINHO \uD83D\uDE0F\uD83D\uDCA6\n");
+
+    private String text;
+
+    Messages(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
