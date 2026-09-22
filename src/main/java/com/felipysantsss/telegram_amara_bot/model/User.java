40f3b74ed52a4b3b47db8b788953b16d6030fdfe
FeSantsss <tercetime18@gmail.com>
2026-09-22 17:22:27 -0300

    fix: add news atributes and annotations on User class

--- src/main/java/com/felipysantsss/telegram_amara_bot/model/User.java
+++ src/main/java/com/felipysantsss/telegram_amara_bot/model/User.java
@@ -1,18 +1,40 @@
 package com.felipysantsss.telegram_amara_bot.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import com.felipysantsss.telegram_amara_bot.enums.Plans;
+import com.felipysantsss.telegram_amara_bot.enums.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 // usuario que irá comprar os planos e receber as fotos.
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
+    private String chatId;
+
+    private String userName;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    @Enumerated(EnumType.STRING)
+    private Plans userPlan;
+
+    private LocalDateTime planExpiresAt;
 
 }
