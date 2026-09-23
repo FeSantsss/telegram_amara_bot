9d6c473ca70f644793227b17934e38b466064a29
FeSantsss <tercetime18@gmail.com>
2026-09-23 19:34:21 -0300

    feat/switch initialization implemented with message saying the plan, plan value and showing the copia e cola and QR Code

--- src/main/java/com/felipysantsss/telegram_amara_bot/Utils/ChosenPlan.java
+++ src/main/java/com/felipysantsss/telegram_amara_bot/Utils/ChosenPlan.java
@@ -1,9 +1,20 @@
 package com.felipysantsss.telegram_amara_bot.Utils;
 
+import com.felipysantsss.telegram_amara_bot.enums.Messages;
 import com.felipysantsss.telegram_amara_bot.enums.Plans;
+import com.felipysantsss.telegram_amara_bot.enums.UserStatus;
+import com.felipysantsss.telegram_amara_bot.model.User;
 import com.felipysantsss.telegram_amara_bot.repository.UserRepository;
+import com.mercadopago.exceptions.MPApiException;
+import com.mercadopago.exceptions.MPException;
+import com.mercadopago.resources.order.Order;
 import lombok.NonNull;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import static com.felipysantsss.telegram_amara_bot.Utils.Base64ToQrCodeConverter.convertQrCode;
 
 public class ChosenPlan {
     public static void chosenPlan(
@@ -16,12 +27,135 @@ public class ChosenPlan {
         switch (chosenPlanCallBack){
             case "5days_plan" -> {
                 Plans plan = Plans.INTERESSADO_PLAN;
+                try{
+                    User client = userRepository.findByChatId(chatId).get();
+                    client.setUserStatus(UserStatus.WAITING_PAYMENT);
+                    userRepository.save(client);
+                    Order order = CreatePayment.paymentGenerator(plan, synthenticEmail, chatId);
+
+                    String messageReplaced = Messages.CHOSING_PLAN.getText()
+                                    .replace("{chosenPlan}", order.getDescription())
+                                            .replace("{orderValue}", order.getCurrency() + order.getTotalAmount());
+
+
+                    MessageSender.MessageSender(chatId, messageReplaced, telegramClient);
+                    MessageSender.MessageSender(chatId, "Copia e cola:", telegramClient);
+                    String copiaECola = order.getTransactions()
+                                    .getPayments()
+                                            .getFirst()
+                                                    .getPaymentMethod()
+                                                            .getQrCode();
+
+                    MessageSender.MessageSender(chatId, copiaECola, telegramClient);
+
+                    String qrCodeInBase64 = order
+                            .getTransactions()
+                            .getPayments()
+                            .getFirst()
+                            .getPaymentMethod()
+                            .getQrCodeBase64();
+
+                    InputFile qrCode = Base64ToQrCodeConverter.convertQrCode(qrCodeInBase64);
+                    SendPhoto sendQrCode = SendPhoto.builder()
+                            .chatId(chatId)
+                            .photo(qrCode)
+                            .build();
+
+                    telegramClient.execute(sendQrCode);
+
+                } catch (MPException | MPApiException e) {
+                    throw new RuntimeException(e);
+                } catch (TelegramApiException e){
+                    System.out.println(e.getMessage());
+                }
             }
             case "20days_safado_plan" -> {
                 Plans plan = Plans.SAFADO_PLAN;
+                try{
+                    User client = userRepository.findByChatId(chatId).get();
+                    client.setUserStatus(UserStatus.WAITING_PAYMENT);
+                    userRepository.save(client);
+                    Order order = CreatePayment.paymentGenerator(plan, synthenticEmail, chatId);
+
+                    String messageReplaced = Messages.CHOSING_PLAN.getText()
+                            .replace("{chosenPlan}", order.getDescription())
+                            .replace("{orderValue}", order.getCurrency() + order.getTotalAmount());
+
+
+                    MessageSender.MessageSender(chatId, messageReplaced, telegramClient);
+                    MessageSender.MessageSender(chatId, "Copia e cola:", telegramClient);
+                    String copiaECola = order.getTransactions()
+                            .getPayments()
+                            .getFirst()
+                            .getPaymentMethod()
+                            .getQrCode();
+
+                    MessageSender.MessageSender(chatId, copiaECola, telegramClient);
+
+                    String qrCodeInBase64 = order
+                            .getTransactions()
+                            .getPayments()
+                            .getFirst()
+                            .getPaymentMethod()
+                            .getQrCodeBase64();
+
+                    InputFile qrCode = Base64ToQrCodeConverter.convertQrCode(qrCodeInBase64);
+                    SendPhoto sendQrCode = SendPhoto.builder()
+                            .chatId(chatId)
+                            .photo(qrCode)
+                            .build();
+
+                    telegramClient.execute(sendQrCode);
+
+                } catch (MPException | MPApiException e) {
+                    throw new RuntimeException(e);
+                } catch (TelegramApiException e){
+                    System.out.println(e.getMessage());
+                }
             }
             case "30days_vip_plan" -> {
                 Plans plan = Plans.VIP_PLAN;
+                try{
+                    User client = userRepository.findByChatId(chatId).get();
+                    client.setUserStatus(UserStatus.WAITING_PAYMENT);
+                    userRepository.save(client);
+                    Order order = CreatePayment.paymentGenerator(plan, synthenticEmail, chatId);
+
+                    String messageReplaced = Messages.CHOSING_PLAN.getText()
+                            .replace("{chosenPlan}", order.getDescription())
+                            .replace("{orderValue}", order.getCurrency() + order.getTotalAmount());
+
+
+                    MessageSender.MessageSender(chatId, messageReplaced, telegramClient);
+                    MessageSender.MessageSender(chatId, "Copia e cola:", telegramClient);
+                    String copiaECola = order.getTransactions()
+                            .getPayments()
+                            .getFirst()
+                            .getPaymentMethod()
+                            .getQrCode();
+
+                    MessageSender.MessageSender(chatId, copiaECola, telegramClient);
+
+                    String qrCodeInBase64 = order
+                            .getTransactions()
+                            .getPayments()
+                            .getFirst()
+                            .getPaymentMethod()
+                            .getQrCodeBase64();
+
+                    InputFile qrCode = Base64ToQrCodeConverter.convertQrCode(qrCodeInBase64);
+                    SendPhoto sendQrCode = SendPhoto.builder()
+                            .chatId(chatId)
+                            .photo(qrCode)
+                            .build();
+
+                    telegramClient.execute(sendQrCode);
+
+                } catch (MPException | MPApiException e) {
+                    throw new RuntimeException(e);
+                } catch (TelegramApiException e){
+                    System.out.println(e.getMessage());
+                }
             }
         }
 
--- src/main/java/com/felipysantsss/telegram_amara_bot/enums/Messages.java
+++ src/main/java/com/felipysantsss/telegram_amara_bot/enums/Messages.java
@@ -19,7 +19,40 @@ public enum Messages {
             "\n" +
             "Você aguenta entrar nesse jogo comigo?\n" +
             "\n" +
-            "VEM PRO MEU CANTINHO \uD83D\uDE0F\uD83D\uDCA6\n");
+            "VEM PRO MEU CANTINHO \uD83D\uDE0F\uD83D\uDCA6\n"),
+
+    CHOSING_PLAN("✨ Prontinho, meu bem! ✨\n" +
+            "\n" +
+            "Você escolheu o plano: {chosenPlan}.\n" +
+            "Valor: {orderValue}\n" +
+            "\n" +
+            "Agora é só finalizar o pagamento pelo QR Code abaixo ou usar o código copia e cola:\n" +
+            "\n" +
+            "Assim que o pagamento for confirmado, seu plano será liberado para você. \uD83D\uDDA4\n" +
+            "\n" +
+            "Tô te esperando… \uD83D\uDE0F\uD83D\uDC8B"),
+
+    RECUSED_PAYMENT("Ops, meu amor… \uD83E\uDD7A\uD83D\uDDA4\n" +
+            "Seu pagamento foi cancelado ou recusado, então a assinatura não foi ativada.\n" +
+            "Se ainda quiser o seu plano, é só tentar novamente. ✨\n" +
+            "Plano: {chosenPlan}\n" +
+            "Quando estiver tudo certinho, eu vou estar te esperando por aqui. \uD83D\uDC8B"),
+
+
+    EXPIRED_PAYMENT("Ei, meu amor… seu pagamento acabou expirando. ⏰\uD83D\uDDA4\n" +
+            "Isso significa que o prazo para concluir a compra terminou e o plano não foi ativado.\n" +
+            "Mas fica tranquila… se você ainda quiser, é só fazer uma nova tentativa. \uD83D\uDE09✨\n" +
+            "Vou estar aqui te esperando. \uD83D\uDC8B"),
+
+    SUCESSED_BUY("Obrigada pela sua compra, meu amor. \uD83D\uDDA4\n" +
+            "Fico muito feliz em ter você por aqui. Espero que você aproveite cada detalhe do seu plano… e que volte sempre. \uD83D\uDE0F✨\n" +
+            "Um beijo da Amara. \uD83D\uDC8B"),
+
+    EXPIRED_PLAN("Hmm… seu plano chegou ao fim, meu amor. \uD83D\uDDA4\n" +
+            "Mas não precisa ficar com saudade de mim… \uD83D\uDE0F\n" +
+            "Se quiser continuar comigo e ter acesso novamente, é só assinar um novo plano.\n" +
+            "Vem renovar sua assinatura e ficar pertinho de mim mais uma vez. \uD83D\uDC8B✨\n" +
+            "Quero você por aqui novamente.");
 
     private String text;
 
