617a2f11150cdcc5741cbb746e2875628f0dd9bc
FeSantsss <tercetime18@gmail.com>
2026-09-23 20:32:03 -0300

    fix: add the channel IDs

--- src/main/java/com/felipysantsss/telegram_amara_bot/enums/Plans.java
+++ src/main/java/com/felipysantsss/telegram_amara_bot/enums/Plans.java
@@ -9,40 +9,34 @@ public enum Plans {
             "\uD83C\uDF38 INTERESSADO Por R$6.90 (5 dias) - LINGERIE \uD83D\uDD1E",
             "5days_plan",
             5,
-            new ArrayList<>(
-                    List.of("")
-            ),
+            -1004414302797L,
             new BigDecimal("6.90")),
 
     SAFADO_PLAN(
             "\uD83D\uDC8E SAFADO por R$ 10.90* (20 dias) + SURPRESINHA \uD83C\uDF81",
             "20days_safado_plan",
             20,
-            new ArrayList<>(
-                    List.of("")
-            ),
+            -1004321327492L,
             new BigDecimal("10.90")),
 
     VIP_PLAN(
             "\uD83D\uDC51 VIP+ por R$ 20.90 (30 dias) - VÍDEOS EXCLUSIVOS",
             "30days_vip_plan",
             30,
-            new ArrayList<>(
-                    List.of("")
-            ),
+            -1004434426417L,
             new BigDecimal("20.90"));
 
     String text;
     String callBack;
     Integer daysToExpiration;
-    List<String> medias;
+    Long channelId;
     BigDecimal price;
 
-    Plans(String text, String callBack, Integer daysToExpiration, List<String> medias, BigDecimal price) {
+    Plans(String text, String callBack, Integer daysToExpiration, Long channelId, BigDecimal price) {
         this.text = text;
         this.callBack = callBack;
         this.daysToExpiration = daysToExpiration;
-        this.medias = medias;
+        this.channelId = channelId;
         this.price = price;
     }
 
@@ -58,8 +52,8 @@ public enum Plans {
         return daysToExpiration;
     }
 
-    public List<String> getMedias() {
-        return medias;
+    public Long getCanalId() {
+        return channelId;
     }
 
     public BigDecimal getPrice() {
