18999b0ec45eaab97766f3fbd6d2fcfb4dc84432
FeSantsss <tercetime18@gmail.com>
2026-09-22 23:27:40 -0300

    fix/add plans price in BigDecimal

--- src/main/java/com/felipysantsss/telegram_amara_bot/enums/Plans.java
+++ src/main/java/com/felipysantsss/telegram_amara_bot/enums/Plans.java
@@ -1,5 +1,6 @@
 package com.felipysantsss.telegram_amara_bot.enums;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +11,8 @@ public enum Plans {
             5,
             new ArrayList<>(
                     List.of("")
-            )),
+            ),
+            new BigDecimal("6.90")),
 
     SAFADO_PLAN(
             "\uD83D\uDC8E SAFADO por R$ 10.90* (20 dias) + SURPRESINHA \uD83C\uDF81",
@@ -18,7 +20,8 @@ public enum Plans {
             20,
             new ArrayList<>(
                     List.of("")
-            )),
+            ),
+            new BigDecimal("10.90")),
 
     VIP_PLAN(
             "\uD83D\uDC51 VIP+ por R$ 20.90 (30 dias) - VÍDEOS EXCLUSIVOS",
@@ -26,18 +29,21 @@ public enum Plans {
             30,
             new ArrayList<>(
                     List.of("")
-            ));
+            ),
+            new BigDecimal("20.90"));
 
     String text;
     String callBack;
     Integer daysToExpiration;
     List<String> medias;
+    BigDecimal price;
 
-    Plans(String text, String callBack, Integer daysToExpiration, List<String> medias) {
+    Plans(String text, String callBack, Integer daysToExpiration, List<String> medias, BigDecimal price) {
         this.text = text;
         this.callBack = callBack;
         this.daysToExpiration = daysToExpiration;
         this.medias = medias;
+        this.price = price;
     }
 
     public String getText() {
@@ -54,5 +60,9 @@ public enum Plans {
 
     public List<String> getMedias() {
         return medias;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
     }
 }
